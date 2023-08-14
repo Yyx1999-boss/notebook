696376aa90d36fff9ba39d2a2f1b1abd420e2c7c
yyx <1351587498@qq.com>
2023-08-14 18:10:07 +0800

    add ignore

---
+++ components/Signcpn/Signcpn.vue
@@ -0,0 +1,94 @@
+<template>
+	<view class="signbox">
+		<view class="signinnerbox">
+			<view class="signuser_box">
+				<up-input fontSize="18" placeholder="请输入账号" border="surround" v-model="value" @change="change"></up-input>
+			</view>
+			<view class="signpwd_box">
+				<up-input
+					fontSize="18"
+					suffixIcon="eye-off"
+					suffixIconStyle="font-size: 22px;color: black"
+					placeholder="请输入密码"
+					border="surround"
+					v-model="value"
+					@change="change"
+				></up-input>
+				<view class="singbtn"></view>
+			</view>
+			<up-button class="signButton" type="primary" text="登录"></up-button>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'Signcpn',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style scoped lang="scss">
+.signbox {
+	position: fixed;
+	width: 700rpx;
+	height: 600rpx;
+	// background-color: white;
+	border-radius: 50px;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	.signuser_box {
+		width: 600rpx;
+		height: 110rpx;
+		display: flex;
+		align-items: center;
+		margin-bottom: 21px;
+		background-color: #f4f4f4;
+		border-radius: 50px;
+		padding-left: 20px;
+		box-sizing: border-box;
+	}
+	.signpwd_box {
+		width: 600rpx;
+		height: 110rpx;
+		display: flex;
+		align-items: center;
+		background-color: #f4f4f4;
+		border-radius: 50px;
+		margin-bottom: 21px;
+		padding-left: 20px;
+		padding-right: 10px;
+		box-sizing: border-box;
+		position: relative;
+		.singbtn {
+			width: 80rpx;
+			height: 80rpx;
+			position: absolute;
+			bottom: 0;
+			right: 0;
+			background-color: transparent;
+			border: none !important;
+		}
+	}
+	.signButton {
+		width: 600rpx;
+		border-radius: 50rpx;
+		height: 100rpx;
+		background-color: #ff93dd;
+		border: none;
+		font-size: 16px;
+		font-weight: 600;
+
+		::v-deep .u-button__text {
+			font-size: 16px !important;
+		}
+	}
+}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="sign_box">
+		<!-- <up-button class="signbutton" text="登录"></up-button> -->
+		<Signcpn></Signcpn>
+	</view>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+const model1 = reactive({
+	username: '',
+	password: ''
+});
+
+const rules = reactive({
+	username: {
+		required: true,
+		message: '请填写用户名',
+		trigger: ['blur', 'change']
+	},
+	password: {
+		required: true,
+		message: '请填写密码',
+		trigger: ['blur', 'change']
+	}
+});
+</script>
+
+<style scoped lang="scss">
+.sign_box {
+	width: 100vw;
+	height: 100vh;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	background-image: url(https://notebookbucket.oss-cn-hangzhou.aliyuncs.com/mobile/main_page/back.png);
+	background-repeat: no-repeat;
+	background-size: cover;
+	.signbutton {
+		width: 400rpx;
+		height: 100rpx;
+		border-radius: 50rpx;
+		position: fixed;
+		bottom: 300rpx;
+	}
+}
+</style>
